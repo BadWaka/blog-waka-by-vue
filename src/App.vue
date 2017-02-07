7b39adfd3833080dc1af02ae70b702c41fdb0224
BadWaka <1456683844@qq.com>
2017-02-07 08:50:21 +0800

    reset.css

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <h1>Hello Waka</h1>
     <router-view></router-view>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+// 在ESLint规定中，如果new了一个新对象，是必须要赋值给某一个变量的，否则会编译报错
+// 但是 new Vue 是不需要赋值给变量的，这时候就需要一个额外的例外
+// 而 /* eslint-disable no-new */ 这段注释的意思就是给下方的代码一个特例，下方的代码在ESLint检查的时候可以忽略no-new这个错误
+// 所以 /* eslint-disable no-new */ 这个注释很重要，不能删除
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
