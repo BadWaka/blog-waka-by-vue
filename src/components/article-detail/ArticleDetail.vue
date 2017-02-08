ba485fcc1572d5fa40c5aff3458daff902a79524
BadWaka <1456683844@qq.com>
2017-02-08 14:27:05 +0800

    文章详情页备份

--- src/components/article-detail/ArticleDetail.vue
+++ src/components/article-detail/ArticleDetail.vue
@@ -1,5 +1,6 @@
 <template>
-  <section>
+  <section class="page">
+    <!--appbar-->
     <mu-appbar class="app-bar" title="文章标题">
       <mu-icon-button icon="arrow_back" slot="left" @click="back"/>
       <mu-icon-menu icon="more_vert" slot="right">
@@ -10,6 +11,11 @@
         <mu-menu-item title="菜单 5"/>
       </mu-icon-menu>
     </mu-appbar>
+
+    <!--文章详情-->
+    <mu-paper class="article" :zDepth="2">
+
+    </mu-paper>
   </section>
 </template>
 
@@ -29,7 +35,20 @@
   // 引入颜色css
   @import "../../common/css/color.scss";
 
+  .page {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-bottom: 20px;
+  }
+
   .app-bar {
     background-color: $blue500;
+  }
+
+  .article {
+    width: 90%;
+    height: 500px;
+    margin-top: 48px;
   }
 </style>
