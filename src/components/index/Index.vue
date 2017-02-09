c53c4744d21a6cf791d4c05f357bf06a60b17c8e
BadWaka <1456683844@qq.com>
2017-02-09 09:52:52 +0800

    增加一些注释

--- src/components/index/Index.vue
+++ src/components/index/Index.vue
@@ -1,23 +1,27 @@
 <template>
-
   <!--index页整个容器-->
   <section>
 
-    <!--顶部抽屉按钮和github-->
+    <!--顶部工具栏 使用fixed固定在顶部-->
     <div class="top">
+      <!--左侧抽屉开关-->
       <mu-icon-button @click="drawerToggle">
         <i class="material-icons">list</i>
       </mu-icon-button>
+      <!--右侧GitHub链接-->
       <mu-icon-button @click="github">
         <i class="iconfont icon-github"></i>
       </mu-icon-button>
     </div>
 
-    <!--侧边菜单-->
+    <!--点击抽屉开关打开的侧边菜单-->
     <mu-drawer right :open="isDrawerOpen" @close="drawerToggle">
+      <!--标题栏-->
       <mu-appbar class="drawer-header" title="类别"/>
       <mu-list>
+        <!--类别List-->
         <mu-list-item v-for="type in typeArray" :title="type"/>
+        <!--关闭栏-->
         <mu-list-item @click="drawerToggle" title="关闭"/>
       </mu-list>
     </mu-drawer>
@@ -26,18 +30,18 @@
     <header>
       <!--头像-->
       <div class="avatar"></div>
-
-      <!--提示语-->
+      <!--提示语 暂时写死，以后会从服务器取得-->
       <div class="welcome">Welcome to waka's blog</div>
     </header>
 
     <!--文章列表-->
     <section class="articles">
+      <!--引用子组件 ArticleItem 注意html代码中需要将驼峰转化成小写加短横线的形式-->
+      <!--artcile 是父组件Index.vue传递给子组件ArticleItem.vue的props-->
       <article-item v-for="article in articles" :article="article"></article-item>
     </section>
 
   </section>
-
 </template>
 
 <script>
@@ -54,7 +58,7 @@
     // 数据
     data () {
       return {
-        articles: {}, // 文章数据
+        articles: {}, // 文章数据，请求获得
         isDrawerOpen: false,  // 侧边栏开关
         typeArray: ['HTML', 'CSS', 'Sass', 'Java Script', 'ECMAScript', 'Vue.js', 'React', 'React Native', '微信小程序', 'Node.js', 'MongoDB', 'macOS', 'Linux']  // 类型数组，mock数据，后续会从服务器上取得
       }
@@ -63,20 +67,23 @@
     created () {
       // 请求文章列表数据
       this.$http.get('/api/articles').then(response => {  // 请求成功
-        console.log(response);
         if (response.status !== 200) {
+          // 输出错误信息
           this.articles = 'status = ' + response.status + ' errorCode = ' + response.body.errorCode;
           return;
         }
         this.articles = response.body.data;
-      }, response => {  // 请求失败
-
+      }, response => {  // 请求失败，因为mock数据没有请求失败，所以暂时没有处理
+        this.articles = '请求失败';
       });
     },
+    // 方法
     methods: {
+      // 开关抽屉
       drawerToggle () {
         this.isDrawerOpen = !this.isDrawerOpen;
       },
+      // 跳转到GitHub
       github () {
         window.open('https://github.com/BadWaka');
       }
