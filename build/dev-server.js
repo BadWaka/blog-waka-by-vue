07a79a4de89e93df3f566fbd8d0fea4847aa23f6
BadWaka <1456683844@qq.com>
2017-02-11 12:03:53 +0800

    Article Schema完成

--- build/dev-server.js
+++ build/dev-server.js
@@ -1,45 +1,45 @@
-require('./check-versions')()
+require('./check-versions')();
 
-var config = require('../config')
+let config = require('../config');
 if (!process.env.NODE_ENV) {
   process.env.NODE_ENV = JSON.parse(config.dev.env.NODE_ENV)
 }
 
-var opn = require('opn')
-var path = require('path')
-var express = require('express')
+let opn = require('opn');
+let path = require('path');
+let express = require('express');
 
-var webpack = require('webpack')
+let webpack = require('webpack');
 // http 代理中间件
-var proxyMiddleware = require('http-proxy-middleware')
-var webpackConfig = process.env.NODE_ENV === 'testing'
+let proxyMiddleware = require('http-proxy-middleware');
+let webpackConfig = process.env.NODE_ENV === 'testing'
   ? require('./webpack.prod.conf')
-  : require('./webpack.dev.conf')
+  : require('./webpack.dev.conf');
 
 // default port where dev server listens for incoming traffic
-var port = process.env.PORT || config.dev.port
+let port = process.env.PORT || config.dev.port;
 // automatically open browser, if not set will be false
-var autoOpenBrowser = !!config.dev.autoOpenBrowser
+let autoOpenBrowser = !!config.dev.autoOpenBrowser;
 // Define HTTP proxies to your custom API backend
 // https://github.com/chimurai/http-proxy-middleware
-var proxyTable = config.dev.proxyTable
+let proxyTable = config.dev.proxyTable;
 
-var app = express()
-var compiler = webpack(webpackConfig)
+let app = express();
+let compiler = webpack(webpackConfig);
 
 
 /*********************************************************************/
 // TODO 编写本地服务 使用mock数据
 
 // 引入data.json
-var appData = require('../data.json');
-var articles = appData.articles;  // 拿到文章列表数据
+let appData = require('../data.json');
+let articles = appData.articles;  // 拿到文章列表数据
 
 // 因为要读取.md文件，所以引入文件读取模块fs
-var fs = require('fs');
+let fs = require('fs');
 
 // 定义Express的路由，并编写接口
-var apiRoutes = express.Router();
+let apiRoutes = express.Router();
 
 // 请求文章列表
 apiRoutes.get('/articles', function (req, res) {
@@ -53,7 +53,7 @@ apiRoutes.get('/articles', function (req, res) {
 // 请求具体的某一篇文章
 apiRoutes.get('/article/1', function (req, res) {
   // 文件路径
-  var mdPath = path.join(__dirname, '../article1.md');
+  let mdPath = path.join(__dirname, '../article1.md');
   fs.readFile(mdPath, {
     encoding: 'utf-8'
   }, function (err, data) {
@@ -61,75 +61,73 @@ apiRoutes.get('/article/1', function (req, res) {
       console.log(err);
       return;
     }
-    var mdStr = data;
-    // console.log('mdStr = ' + mdStr);
     res.json({
       errorCode: 0,
-      data: mdStr
+      data: data
     });
   });
 });
 
-// 使用该路由；所有的路由都要加上/api，举个栗子：localhost:8080/api/articles
-app.use('/api', apiRoutes);
+// 使用该路由；所有的路由都要加上/blogWaka，举个栗子：localhost:8080/blogWaka/articles
+app.use('/blogWaka', apiRoutes);
 /*********************************************************************/
 
 
-var devMiddleware = require('webpack-dev-middleware')(compiler, {
+let devMiddleware = require('webpack-dev-middleware')(compiler, {
   publicPath: webpackConfig.output.publicPath,
   quiet: true
-})
+});
 
-var hotMiddleware = require('webpack-hot-middleware')(compiler, {
+let hotMiddleware = require('webpack-hot-middleware')(compiler, {
   log: () => {
   }
-})
+});
 // force page reload when html-webpack-plugin template changes
 compiler.plugin('compilation', function (compilation) {
   compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
-    hotMiddleware.publish({action: 'reload'})
+    hotMiddleware.publish({action: 'reload'});
     cb()
   })
-})
+});
 
 // proxy api requests
 Object.keys(proxyTable).forEach(function (context) {
-  var options = proxyTable[context]
+  let options = proxyTable[context];
   if (typeof options === 'string') {
     options = {target: options}
   }
   app.use(proxyMiddleware(options.filter || context, options))
-})
+});
 
 // handle fallback for HTML5 history API
-app.use(require('connect-history-api-fallback')())
+app.use(require('connect-history-api-fallback')());
 
 // serve webpack bundle output
-app.use(devMiddleware)
+app.use(devMiddleware);
 
 // enable hot-reload and state-preserving
 // compilation error display
-app.use(hotMiddleware)
+app.use(hotMiddleware);
 
 // serve pure static assets
-var staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory)
-app.use(staticPath, express.static('./static'))
+let staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory);
+app.use(staticPath, express.static('./static'));
 
-var uri = 'http://localhost:' + port
+let uri = 'http://localhost:' + port;
 
 devMiddleware.waitUntilValid(function () {
-  console.log('> Listening at ' + uri + '\n')
-})
+  console.log('> Listening at ' + uri + '\n');
+});
 
 
 module.exports = app.listen(port, function (err) {
   if (err) {
-    console.log(err)
+    console.log(err);
     return
   }
 
   // when env is testing, don't need open it
   if (autoOpenBrowser && process.env.NODE_ENV !== 'testing') {
-    opn(uri)
+    opn(uri);
   }
-})
+});
---
+++ schemas/article.js
@@ -0,0 +1,60 @@
+/**
+ * Created by BadWaka on 2017/2/11.
+ */
+import mongoose from 'mongoose';
+
+const ArticleSchema = new mongoose.Schema({
+  title: String,  // 文章名
+  intro: String,  // 文章简介
+  link: String,   // 文章链接
+  typeId: String, // 类型id
+  typeName: String, // 类型名
+  img: String,  // 文章图片
+  meta: {
+    createAt: { // 创建时间
+      type: Date,
+      default: Date.now()
+    },
+    updateAt: { // 更新时间
+      type: Date,
+      default: Date.now()
+    }
+  }
+});
+
+// pre()的意思是，每次进行save操作之前，都会调用这个方法
+ArticleSchema.pre('save', (next) => {
+  // 判断数据是否是新添加的
+  if (this.isNew) {
+    // 如果是，则将创建时间和更新时间都设置为当前时间
+    this.meta.createAt = this.meta.updateAt = Date.now();
+  } else {
+    // 否则，只更新更新时间
+    this.meta.updateAt = Date.now();
+  }
+  // 走接下来的流程
+  next();
+});
+
+ArticleSchema.statics = {
+
+  // fetch方法，用来取出数据库里面所有的数据
+  fetch (cb) {
+    return this
+      .find()
+      .sort('meta.updateAt')  // 排序;根据更新时间排序
+      .exec(cb);
+  },
+
+  // 根据id，查询单条数据
+  findById (id, cb) {
+    return this
+      .findOne({
+        _id: id
+      })
+      .exec(cb);
+  }
+};
+
+// 导出模式
+module.exports = ArticleSchema;
